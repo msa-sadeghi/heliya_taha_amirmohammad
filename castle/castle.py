a2e738f6bf6bc5700641df58f9d4ff65a4f4bc64
msa-sadeghi <sadeghi.msai@gmail.com>
2024-06-15 16:24:33 +0330

    a
    a

--- castle/castle.py
+++ castle/castle.py
@@ -5,16 +5,17 @@ class Castle:
         self.image_100 = pygame.image.load("assets/castle/castle_100.png")
         w = self.image_100.get_width()
         h = self.image_100.get_height()
-        self.image_100 = pygame.transform.scale(self.image_100, (w * 0.5, h * 0.5))
+        self.image_100 = pygame.transform.scale(self.image_100, (w * 0.2, h * 0.2))
         self.image_50 = pygame.image.load("assets/castle/castle_50.png")
         w = self.image_50.get_width()
         h = self.image_50.get_height()
-        self.image_50 = pygame.transform.scale(self.image_50, (w * 0.5, h * 0.5))
+        self.image_50 = pygame.transform.scale(self.image_50, (w * 0.2, h * 0.2))
         self.image_25 = pygame.image.load("assets/castle/castle_25.png")
         w = self.image_25.get_width()
         h = self.image_25.get_height()
-        self.image_25 = pygame.transform.scale(self.image_25, (w * 0.5, h * 0.5))
+        self.image_25 = pygame.transform.scale(self.image_25, (w * 0.2, h * 0.2))
         self.image = self.image_100
+        self.rect = self.image.get_rect(topleft = (x,y))
         self.health = 100
         self.max_health = self.health
         self.score = 0
--- castle/main.py
+++ castle/main.py
@@ -1,17 +1,22 @@
 import pygame
 from constants import *
 from world import World
+from castle import Castle
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 world = World()
+
+castle = Castle(SCREEN_WIDTH - 280, SCREEN_HEIGHT - 340)
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    world.draw(screen)        
+    world.draw(screen) 
+    castle.draw(screen)       
     pygame.display.update()
     clock.tick(FPS)
     
