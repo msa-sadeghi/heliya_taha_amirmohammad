11d20d9efc0fa6e4a2d1500e500f5b681f2d6a01
msa-sadeghi <sadeghi.msai@gmail.com>
2024-07-06 16:56:04 +0330

    a

--- review/4.py
+++ review/4.py
@@ -35,16 +35,51 @@
 
 
 
-def write_to_file(f_name, text):
-    with open(f_name, "a") as f:
-        f.write(text)
+# def write_to_file(f_name, text):
+#     with open(f_name, "a") as f:
+#         f.write(text)
         
 
-for i in range(3):
-    name = input("enter a name: ")
-    write_to_file("z.txt", name+"\n")
+# for i in range(3):
+#     name = input("enter a name: ")
+#     write_to_file("z.txt", name+"\n")
 
 #  پروژه ثبت نام مدرسه
 # نام و مشخصات هر دانش آموز را از ورودی دریافت نماید
 # برنامه امکان ثبت دانش آموز را در فایل دارد
 # برنامه امکان جستجو و نمایش اطلاعات دانش آموز را دارد
+# برنامه منو دارد
+# hint :  you must use while loop and store all data in a file
+# 0  ->    exit
+# 1  ->    show all students
+# 2  ->    add new student
+
+import json
+
+# name = input("enter your name: ")
+# age = input("enter your age: ")
+# student = {
+#     "name":name,
+#     "age": age
+# }
+# serialized_str = json.dumps(student)
+# with open("students.txt", "a") as f:
+#     f.write(serialized_str)
+
+
+# with open("students.txt", "r") as f:
+#    text = f.read()
+#    obj = json.loads(text)
+#    print(obj["name"])
+
+def my_function(r, c):
+    if r < 0 or c > 7 or r > 7 or c <0:
+        return "error"
+    if r % 2 == c % 2:
+        return "white"
+    return "black"
+
+print(my_function(0,0))
+print(my_function(0,3))
+print(my_function(1,3))
+print(my_function(4,4))
