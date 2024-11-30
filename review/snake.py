c66d5870748d0306e7922d29e5e46e5d9810ebbd
msa-sadeghi <sadeghi.msai@gmail.com>
2024-11-30 17:03:45 +0330

    Update snake.py

--- review/snake.py
+++ review/snake.py
@@ -16,8 +16,8 @@ class Snake:
 
 class Food:
     def __init__(self) -> None:
-        x = randint(0, GAME_WIDTH // SPACE_SIZE) * SPACE_SIZE
-        y = randint(0, GAME_HEIGHT // SPACE_SIZE) * SPACE_SIZE
+        x = randint(0, GAME_WIDTH // SPACE_SIZE - 1) * SPACE_SIZE
+        y = randint(0, GAME_HEIGHT // SPACE_SIZE - 1) * SPACE_SIZE
         self.coordinates = [x,y]
         canvas.create_oval(x,y, x + SPACE_SIZE, y + SPACE_SIZE, fill="red", tag="food")
 
@@ -46,15 +46,49 @@ def next_turn(snake, food):
         del snake.coordinates[-1]
         canvas.delete(snake.squares[-1])
         del snake.squares[-1]
+    if check_game_over():
+        game_over()
+    else:
+        window.after(200, next_turn, snake, food)
+   
+   
     
-    
+def check_game_over():
+    x,y = snake.coordinates[0]
+    if x < 0 or x > GAME_WIDTH or y < 0 or y > GAME_HEIGHT:
+        return True
+    return False
+
+
+def game_over():
+    canvas.delete("all")
+    canvas.create_text(canvas.winfo_width()/2, canvas.winfo_height()/2, font=("arial", 22),fill="red", text="Game Over")
+    del snake
+    del food
+
+
+def change_direction(new_dir)    :
+    global direction
+    if new_dir == "left":
+        if direction != "right":
+            direction = "left"
+    elif new_dir == "right":
+        if direction != "left":
+            direction = "right"
+    elif new_dir == "up":
+        if direction != "down":
+            direction = "up"
+    elif new_dir == "down":
+        if direction != "up":
+            direction = "down"
+            
 
 
 
 BODY_SIZE = 2
 SPACE_SIZE = 50
 GAME_WIDTH = 700
-GAME_HEIGHT = 700
+GAME_HEIGHT = 500
 direction = "down"
 score = 0
 window = Tk()
@@ -66,7 +100,8 @@ score_label.pack()
 canvas = Canvas(window, bg="black", width=GAME_WIDTH, height=GAME_HEIGHT)
 canvas.pack()
 
-
+restart_button = Button(window, text="restart")
+restart_button.pack()
 window.update()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
@@ -78,7 +113,13 @@ y = screen_height//2 - window_height//2
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
+window.bind('<Up>',lambda e: change_direction('up'))
+window.bind('<Down>',lambda e: change_direction('down'))
+window.bind('<Left>',lambda e: change_direction('left'))
+window.bind('<Right>',lambda e: change_direction('right'))
+
 snake = Snake()
 food = Food()
 next_turn(snake, food)
+
 window.mainloop()
