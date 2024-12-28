178bd4232431ef0c1a262dbd58e140e4e38e1de9
msa-sadeghi <sadeghi.msai@gmail.com>
2024-12-28 16:57:27 +0330

    animation_idle_and_run added

--- platformer2/main.py
+++ platformer2/main.py
@@ -3,12 +3,33 @@ from person import Person
 clock = pygame.time.Clock()
 
 player = Person('player', 100, 300, 60, 10)
-
+moving_left = False
+moving_right = False
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    player.draw(screen)        
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                moving_left = True
+            if event.key == pygame.K_RIGHT:
+                moving_right = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                moving_left = False
+            if event.key == pygame.K_RIGHT:
+                moving_right = False
+    if player.alive:
+        if moving_left or moving_right:
+            player.change_animation_type("Run")
+        else:
+            player.change_animation_type("Idle")
+        
+    
+    screen.fill((0,0,0))        
+    player.draw(screen)   
+    player.move(moving_left, moving_right)     
     pygame.display.update()
     clock.tick(FPS)
--- platformer2/person.py
+++ platformer2/person.py
@@ -1,8 +1,11 @@
 from pygame.sprite import Sprite
 from config import *
 import os
+
+
 class Person(Sprite):
-    def __init__(self, type_, x,y, ammo, grenades):
+    def __init__(self, type_, x, y, ammo, grenades):
+        super().__init__()
         self.alive = True
         self.health = 100
         self.max_health = 100
@@ -13,24 +16,53 @@ class Person(Sprite):
         self.animation_types = os.listdir(f"assets/images/{type_}")
         for animation in self.animation_types:
             temp_list = []
-            num_of_images = len(os.listdir(f"assets/images/{type_}/{animation}"))
+            num_of_images = len(os.listdir(
+                f"assets/images/{type_}/{animation}"))
             for i in range(num_of_images):
-                img =pygame.image.load(f"assets/images/{type_}/{animation}/{i}.png")
+                img = pygame.image.load(
+                    f"assets/images/{type_}/{animation}/{i}.png")
                 img_w = img.get_width()
                 img_h = img.get_height()
-                img = pygame.transform.scale(img, (img_w * 1.5, img_h * 1.5))
+                img = pygame.transform.scale(img, (img_w * 1.8, img_h * 1.8))
                 temp_list.append(img)
-                
+
             self.image_dict[animation] = temp_list
         self.image_number = 0
         self.action = self.animation_types[1]
         self.image = self.image_dict[self.action][0]
-        self.rect = self.image.get_rect(topleft=(x,y))
-        
-    def draw(self, screen)  :
-        screen.blit(self.image, self.rect)
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.last_image_change_time = 0
+        self.firection = 1
+        self.flip = False
 
-            
-                
-        
-        
+    def draw(self, screen):
+        self.animation()
+        img = self.image_dict[self.action][self.image_number]
+        img = pygame.transform.flip(img, self.flip, False)
+        screen.blit(img, self.rect)
+
+    def animation(self):
+        if pygame.time.get_ticks() - self.last_image_change_time > 100:
+            self.last_image_change_time = pygame.time.get_ticks()
+            self.image_number += 1
+            if self.image_number >= len(self.image_dict[self.action]):
+                self.image_number = 0
+
+    def move(self, moving_left, moving_right):
+        dx = 0
+        if moving_left:
+            self.direction = -1
+            self.flip = True
+            dx -= 5
+        elif moving_right:
+            self.direction = 1
+            self.flip = False
+            dx += 5
+
+        self.rect.x += dx
+
+    def change_animation_type(self, new_type):
+        if new_type != self.action:
+            self.action = new_type
+            self.image_number = 0
+            self.last_image_change_time = 0
